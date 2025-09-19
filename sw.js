b36ff36386c3d54cbae9398ab9b3915298a8c31c
barneyme <barney@barney.me>
2025-09-19 07:40:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,46 +1,43 @@
-const CACHE_NAME = "editr-v1";
-const urlsToCache = [
-  "./",
-  "./index.html", // Caching the root URL as index.html
-  "./manifest.json",
-  "https://cdn.tailwindcss.com",
-  // Cache the icons from the manifest file
-  "https://placehold.co/192x192/282a36/f8f8f2?text=e",
-  "https://placehold.co/512x512/282a36/f8f8f2?text=e",
-  "https://placehold.co/512x512/282a36/f8f8f2?text=b&maskable=true",
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }),
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return the response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    }),
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        }),
-      );
-    }),
-  );
-});
+{
+  "name": "editr",
+  "short_name": "editr",
+  "description": "Text editor.",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#282a36",
+  "theme_color": "#44475a",
+  "icons": [
+    {
+      "src": "https://placehold.co/192x192/282a36/f8f8f2?text=e",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://placehold.co/512x512/282a36/f8f8f2?text=e",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "https://placehold.co/512x512/282a36/f8f8f2?text=b&maskable=true",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ],
+  "file_handlers": [
+    {
+      "action": "/index.html",
+      "accept": {
+        "text/plain": [".txt"]
+      },
+      "icons": [
+        {
+          "src": "https://placehold.co/192x192/282a36/f8f8f2?text=txt",
+          "sizes": "192x192",
+          "type": "image/png"
+        }
+      ],
+      "launch_type": "single-client"
+    }
+  ]
+}
